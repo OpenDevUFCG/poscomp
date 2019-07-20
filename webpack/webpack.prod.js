ccbecb86985469f397bce02d6e22dce22a6a7039
joao.espindula@ccc.ufcg.edu.br <joao.espindula@ccc.ufcg.edu.br>
2019-07-20 18:02:37 -0300

    Remove webpack

--- webpack/webpack.prod.js
+++
@@ -1,7 +0,0 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const common = require('./webpack.common');
-
-module.exports = merge(common, {
-    mode: 'production'
-});
