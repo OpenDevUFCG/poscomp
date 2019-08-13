004140ebf9589c7fbc19ef82cd84cd22c5f7fe6d
LukeHxH <lucasmed812@gmail.com>
2019-08-13 14:25:55 -0300

    Remove componente NavContent

--- src/components/sidenav/PageSideNav.js
+++ src/components/sidenav/PageSideNav.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './PageSideNav.css';
-import NavContent from './NavContent/NavContent';
 
 const PageSideNav = ({ shown }) => {
   let classes = (shown) ? ['drawer', 'open'] : ['drawer'];
 
   return(
     <aside className={classes.join(' ')} >
-      <NavContent />
+      <div className="side-nav-content">
+        <p className="filters-title">Filtros:</p>
+        {/* Adicionar filtros da aplicação. */}
+      </div>
     </aside>
   );
 };
