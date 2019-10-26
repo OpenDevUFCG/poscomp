0da8fae592a95e9ac326e130e322a6159050343e
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-10-26 18:01:36 -0300

    inicia card

---
+++ src/components/card/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css'
+
+const Card = () => {
+    return (
+    <div className={'term-card__container'}>
+        <h1 className={'term-card__title-1'}>Questão 01 - 2018</h1>
+        <span className={'term-card__subtitle'}>Matemática</span>
+    </div>
+    )
+  };
+  
+export default Card;
