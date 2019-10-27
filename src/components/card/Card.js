a747efd7e8c81fe0a6ef62d8fcb909ee56888128
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-10-26 22:20:47 -0300

    adiciona sombra

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import './Card.css'
+import './Card.css';
+
+import PropType from 'prop-types';
 
 const Card = () => {
     return (
-    <div className={'term-card__container'}>
-        <h1 className={'term-card__title-1'}>Questão 01 - 2018</h1>
-        <span className={'term-card__subtitle'}>Matemática</span>
+    <div className={'card__space'}>
+        <div className={'card__container'}>
+            <h1 className={'card__title-1'}>Questão 02 - 2019</h1>
+            <span className={'card__theme'}>Tema 1</span>
+            <span className={'card__subtitle'}>Matemática</span>
+            <h3 className={'card__title-2'}>Sobre árvores, é correto afirmar que: </h3>
+        </div>
     </div>
-    )
-  };
-  
+    );
+};
+
+Card.PropType = {
+    question: PropType.shape({
+        number: PropType.string,
+        year: PropType.string,
+        area: PropType.string,
+        theme: PropType.string,
+        textQuestion: PropType.string,
+        
+    })
+};
+
 export default Card;
